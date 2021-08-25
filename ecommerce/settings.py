2c15bc44b150474dd5a4329ac60477eb3228211e
nahidulnibir <nibir.nahidul@gmail.com>
2021-08-25 17:52:58 +0600

    checkout todo

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -130,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+MEDIA_URL = '/images/'
